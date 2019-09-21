ab5ca7329f90db303c5058c22abda4ea6181262c
cmd16 <catherine.dejager10@gmail.com>
2019-09-21 15:54:25 -0400

    Create FidgetCube activity, add some buttons and sliders

--- Sensapp/app/src/main/java/edu/calvin/cs262/sensapp/MainActivity.java
+++ Sensapp/app/src/main/java/edu/calvin/cs262/sensapp/MainActivity.java
@@ -2,13 +2,23 @@ package edu.calvin.cs262.sensapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchFidgetCube(View view) {
+        Log.d(LOG_TAG, "Fidget cube started!");
+        Intent intent = new Intent(this, FidgetCube.class);
+        startActivity(intent);
     }
 }
