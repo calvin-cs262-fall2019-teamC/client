b2d0c7e5b44adb10c4d33d058d26005276b297d2
beg23 <beg23@students.calvin.edu>
2019-09-25 20:43:50 -0400

    Create activity selection structure

--- app/src/main/java/edu/calvin/cs262/sensapp/MainActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/MainActivity.java
@@ -2,13 +2,30 @@ package edu.calvin.cs262.sensapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Creates the main activity from which other activities will be selected.
+     *
+     * @param savedInstanceState The Bundle of information which initializes the main activity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Launches the breathing activity when the button is pressed.
+     *
+     * @param view The current View object (the breathe activity button).
+     */
+    public void launchBreatheActivity(View view) {
+        Intent intent = new Intent(this, BreatheActivity.class);
+        startActivity(intent);
     }
 }
