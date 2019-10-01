2318c99dc19a07fa7267d80aebc5403563a856e0
cmd16 <catherine.dejager10@gmail.com>
2019-09-30 23:16:42 -0400

    Add sound for button and switch presses (using audio recorded of my own fidget cube).

--- app/src/main/java/edu/calvin/cs262/sensapp/FidgetCubeActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/FidgetCubeActivity.java
@@ -3,6 +3,7 @@ package edu.calvin.cs262.sensapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,31 +11,41 @@ import android.widget.ImageView;
 public class FidgetCubeActivity extends AppCompatActivity {
     AnimationDrawable SwitchAnimation;
     private boolean switchOn = false;
+    private ImageView switchImage;
+    public MediaPlayer buttonSoundPlayer;
+    public MediaPlayer switchUpPlayer;
+    public MediaPlayer switchDownPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fidget_cube);
 
-        ImageView switchImage = findViewById(R.id.switch_image);
+        switchImage = findViewById(R.id.switch_image);
         switchImage.setBackgroundResource(R.drawable.switch_animation_on);
         SwitchAnimation = (AnimationDrawable) switchImage.getBackground();
 
-        switchImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SwitchAnimation.start();
-                switchOn = ! switchOn;
-                ImageView switchImage = findViewById(R.id.switch_image);
-                if (switchOn) {
-                    switchImage.setBackgroundResource(R.drawable.switch_animation_off);
-                    SwitchAnimation = (AnimationDrawable) switchImage.getBackground();
-                }
-                else {
-                    switchImage.setBackgroundResource(R.drawable.switch_animation_on);
-                    SwitchAnimation = (AnimationDrawable) switchImage.getBackground();
-                }
-            }
-        });
+        buttonSoundPlayer = MediaPlayer.create(this, R.raw.button_click_short);
+        switchUpPlayer = MediaPlayer.create(this, R.raw.switch_up);
+        switchDownPlayer = MediaPlayer.create(this, R.raw.switch_down);
+    }
+
+    public void playButtonClick(View view) {
+        buttonSoundPlayer.start();
+    }
+
+    public void switchClick(View view) {
+        SwitchAnimation.start();
+        switchOn = ! switchOn;
+        if (switchOn) {
+            switchDownPlayer.start();
+            switchImage.setBackgroundResource(R.drawable.switch_animation_off);
+            SwitchAnimation = (AnimationDrawable) switchImage.getBackground();
+        }
+        else {
+            switchUpPlayer.start();
+            switchImage.setBackgroundResource(R.drawable.switch_animation_on);
+            SwitchAnimation = (AnimationDrawable) switchImage.getBackground();
+        }
     }
 }
