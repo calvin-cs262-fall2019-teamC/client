9c10007caca049fa0c9597e2ad60fe8e2e92b133
beg23 <beg23@students.calvin.edu>
2019-10-06 15:22:19 -0400

    Implement nav bar using activities

--- app/src/main/java/edu/calvin/cs262/sensapp/FavoritesActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/FavoritesActivity.java
@@ -2,34 +2,29 @@ package edu.calvin.cs262.sensapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class FavoritesActivity extends AppCompatActivity {
     // idea to store context: https://stackoverflow.com/questions/17917968/get-context-in-non-activity-class
     private Context context = this;
     protected BottomNavigationView navigation_bar;
 
     /**
-     * Creates the main activity from which other activities will be selected.
+     * Creates the history activity from which user activity history will be displayed.
      * Sets up the bottom navigation bar.
      *
-     * @param savedInstanceState The Bundle of information which initializes the main activity.
+     * @param savedInstanceState The Bundle of information which initializes this activity.
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_favorites);
 
         // bottom nav bar, help mainly from:
         // https://medium.com/@hitherejoe/exploring-the-android-design-support-library-bottom-navigation-drawer-548de699e8e0
@@ -40,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.navigation_favorites:
-                                startActivity(new Intent(context, FavoritesActivity.class));
                                 break;
                             case R.id.navigation_main:
+                                startActivity(new Intent(context, MainActivity.class));
                                 break;
                             case R.id.navigation_history:
                                 startActivity(new Intent(context, HistoryActivity.class));
@@ -50,16 +45,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                         return true;
                     }
-        });
-    }
-
-    /**
-     * Launches the breathing activity when the button is pressed.
-     *
-     * @param view The current View object (the breathe activity button).
-     */
-    public void launchBreatheActivity(View view) {
-        Intent intent = new Intent(this, BreatheActivity.class);
-        startActivity(intent);
+                });
     }
 }
--- app/src/main/java/edu/calvin/cs262/sensapp/HistoryActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/HistoryActivity.java
@@ -2,34 +2,29 @@ package edu.calvin.cs262.sensapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity {
     // idea to store context: https://stackoverflow.com/questions/17917968/get-context-in-non-activity-class
     private Context context = this;
     protected BottomNavigationView navigation_bar;
 
     /**
-     * Creates the main activity from which other activities will be selected.
+     * Creates the favorites activity from which favorite activities will be selected.
      * Sets up the bottom navigation bar.
      *
-     * @param savedInstanceState The Bundle of information which initializes the main activity.
+     * @param savedInstanceState The Bundle of information which initializes this activity.
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_history);
 
         // bottom nav bar, help mainly from:
         // https://medium.com/@hitherejoe/exploring-the-android-design-support-library-bottom-navigation-drawer-548de699e8e0
@@ -43,23 +38,13 @@ public class MainActivity extends AppCompatActivity {
                                 startActivity(new Intent(context, FavoritesActivity.class));
                                 break;
                             case R.id.navigation_main:
+                                startActivity(new Intent(context, MainActivity.class));
                                 break;
                             case R.id.navigation_history:
-                                startActivity(new Intent(context, HistoryActivity.class));
                                 break;
                         }
                         return true;
                     }
-        });
-    }
-
-    /**
-     * Launches the breathing activity when the button is pressed.
-     *
-     * @param view The current View object (the breathe activity button).
-     */
-    public void launchBreatheActivity(View view) {
-        Intent intent = new Intent(this, BreatheActivity.class);
-        startActivity(intent);
+                });
     }
 }
