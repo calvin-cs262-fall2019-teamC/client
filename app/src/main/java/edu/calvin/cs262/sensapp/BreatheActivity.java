f5f8becf9406cddf93a708bcbbe49b211ddb4e95
Daniel <djkuiper@comcast.net>
2019-10-17 20:27:50 -0400

    push working audio + onPause() stop audio

--- app/src/main/java/edu/calvin/cs262/sensapp/BreatheActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/BreatheActivity.java
@@ -5,14 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class BreatheActivity extends AppCompatActivity {
 
     //create mediaPlayer
-    public MediaPlayer mediaPlayer;
+    public MediaPlayer musicPlayer;
 
     /**
      * Creates the breathe activity in which the user will be guided to breathe deeply.
@@ -26,24 +25,39 @@ public class BreatheActivity extends AppCompatActivity {
 
         //initialize videoView and its mediaController
         VideoView simpleVideoView = findViewById(R.id.videoView);
-        simpleVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.trees01));
+        simpleVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.breathe_video_trees01));
 
         MediaController mediaController = new MediaController(this);
         simpleVideoView.setMediaController(mediaController);
 
-        //set mediaPlayer to light_waves
-        mediaPlayer = mediaPlayer.create(this, R.raw.light_waves);
+
+        //initialize the musicPlayer
+
+
+        //TODO: <initialize list of videos here, pick a random one
+
+        //set mediaPlayer to breathe_music_lightWaves
+        //make this random later
+        musicPlayer = musicPlayer.create(this, R.raw.breathe_music_waves);
+
+//        musicPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//            @Override
+//            public void onCompletion(MediaPlayer mediaPlayer) {
+
+                //musicPlayer = musicPlayer.create(this, R.raw.breathe_music_waves);
+//            }
+//        });
 
         //start video and run sound
         simpleVideoView.start();
-
-        mediaPlayer.start();
+        musicPlayer.start();
     }
 
     public void onPause(){
+        //stop the music if focus is lost
 
         super.onPause();
-        if(mediaPlayer.isPlaying())
-            mediaPlayer.stop();
+        if(musicPlayer.isPlaying())
+            musicPlayer.stop();
     }
 }
