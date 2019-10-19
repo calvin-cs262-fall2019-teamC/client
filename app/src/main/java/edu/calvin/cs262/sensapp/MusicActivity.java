27a710ea961a3840ba9bccbeabd54b3f74cf945d
cmd16 <catherine.dejager10@gmail.com>
2019-10-19 12:05:06 -0400

    Add a grid layout

--- app/src/main/java/edu/calvin/cs262/sensapp/MusicActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/MusicActivity.java
@@ -2,7 +2,9 @@ package edu.calvin.cs262.sensapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MusicActivity extends AppCompatActivity {
 
