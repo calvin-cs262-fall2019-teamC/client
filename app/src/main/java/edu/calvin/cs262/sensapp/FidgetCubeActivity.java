81b46f8ee311579cfb2edb0b4edda43b94154d75
cmd16 <catherine.dejager10@gmail.com>
2019-10-30 20:00:21 -0400

    Update switch image, remove unused import

--- app/src/main/java/edu/calvin/cs262/sensapp/FidgetCubeActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/FidgetCubeActivity.java
@@ -5,7 +5,6 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
