3378d76d2f42aecf0df088a62a972b5efbd7f283
terpstranick99 <55598288+terpstranick99@users.noreply.github.com>
2019-11-07 15:01:41 -0500

    Beginning the Bubbles activity
    This is the backbone for the bubbles activity

--- app/src/main/java/edu/calvin/cs262/sensapp/BubblesActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/BubblesActivity.java
@@ -11,4 +11,16 @@ public class BubblesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bubbles);
     }
+
+    //TODO Initialize a count variable that counts how many bubbles are on the screen at any given time
+
+    //TODO This function will create bubbles, one bubble at a time, an will randomize trajectory
+    void createBubbles(){
+
+    }
+
+    //TODO This function will display a popped bubble image when a bubble is clicked, and it will reduce the count of bubbles by one, prompting another bubble to be created
+    void onClick() {
+
+    }
 }
