44f9b278c3e3e24e6064e97b176d5ecc8872a897
Daniel <djkuiper@comcast.net>
2019-11-14 13:05:13 -0500

    nothing much here really but i need a new branch

--- app/src/main/java/edu/calvin/cs262/sensapp/BubblesActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/BubblesActivity.java
@@ -10,5 +10,13 @@ public class BubblesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bubbles);
+
+        createBubble();
     }
+
+    public void createBubble(){
+
+    }
+
+
 }
