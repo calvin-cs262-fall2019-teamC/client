a2ca576d0bd1b72c8675e75935364c42fccdedf2
terpstranick99 <55598288+terpstranick99@users.noreply.github.com>
2019-11-21 14:45:13 -0500

    Documented Files
    I documented Animals Activity and Stories Activity

--- app/src/main/java/edu/calvin/cs262/sensapp/StoriesActivity.java
+++ app/src/main/java/edu/calvin/cs262/sensapp/StoriesActivity.java
@@ -4,8 +4,20 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * This is another class that is unfinished and unimplemented
+ * When done, this class will create an activity in which
+ * the user can listen to fairytales and other stories
+ * there may also be a visual component
+ */
+
 public class StoriesActivity extends AppCompatActivity {
 
+    /**
+     * When created, inititalize data
+     * @param savedInstanceState The Bundle of data to initialize.
+     * Assign "activity_stories" layout
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
